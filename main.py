f2fea4b13e24a02957bee702742f981840b51105
sumant <scschaudhary81@gmail.com>
2026-05-31 15:49:39 +0530

    Added new packages

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from langchain_ollama import OllamaLLM
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+
 def main():
-    print("Hello from custom-agents!")
+    print(os.getenv("GOOGLE_API_KEY"))
 
 
 if __name__ == "__main__":
     main()
+
