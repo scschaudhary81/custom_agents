c76c43be6ec168b33a2819a313d9363faacf2190
scschaudhary81 <scschaudhary81@gmail.com>
2026-07-19 11:02:14 +0530

    Edit : Final Push

--- main/agents/search_agent.py
+++ main/agents/search_agent.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
 from langchain.agents import create_agent
+from langchain.agents.structured_output import ToolStrategy, ProviderStrategy
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import AnyMessage, HumanMessage
 from langchain_tavily import TavilySearch
 from main.custom_response_schemas.SearchResponseAgent import SearchResponseAgent
 
 load_dotenv()
+MODEL_NAMES = {"google": "google_genai:gemini-3.5-flash", "ollama": "ollama:gemma4:31b-cloud"}
 
 
 class SearchAgent:
@@ -18,6 +20,7 @@ class SearchAgent:
         max_retries: int = 10,
         timeout: int = 120,
     ) -> None:
+        self.model_name = model_name
         self.model = init_chat_model(model_name, max_retries=max_retries, timeout=timeout)
         self.tools = [TavilySearch()]
         self.agent = create_agent(
@@ -32,11 +35,15 @@ class SearchAgent:
 
         messages: list[AnyMessage] = [HumanMessage(content=query)]
         agent_response = self.agent.invoke({"messages": messages})
-        result : SearchResponseAgent = agent_response["structured_response"]
-        return result.model_dump_json(indent=4)
+
+        if self.model_name.__contains__("ollama"):
+            return agent_response
+        else:
+            structured = agent_response.get("structured_response")
+            return structured.model_dump_json(indent=4)
 
 
 if __name__ == "__main__":
-    agent = SearchAgent(model_name="google_genai:gemini-3.5-flash")
+    agent = SearchAgent(model_name=MODEL_NAMES["google"])
     response = agent.search("Give me 3 Software Engineer job recommendations from linkedin")
     print(response)
